aab4ce4ad4487e1cb425fbd82154ee65df9285b5
woshichenwei <3300487834@qq.com>
2019-05-20 21:22:10 +0800

    新增master

--- src/test/java/Master.java
+++ src/test/java/Master.java
@@ -1,6 +1,12 @@
 public class Master {
 
     public void test2(){
-        System.out.println("test2");
+        System.out.println(new Dog());
     }
+}
+class  Dog{
+    String name="大黄";
+    Dog d = new Dog();
+
+
 }
